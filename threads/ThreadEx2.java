4017491d8147a1197ef13b09abf66573fa792016
madhudamsani <126312099+Madhu2111@users.noreply.github.com>
2025-02-04 10:18:16 +0530

    Update ThreadEx2.java

--- threads/ThreadEx2.java
+++ threads/ThreadEx2.java
@@ -1,31 +1,41 @@
 package com.ThreadExamples;
 
-public class ThreadEx2 {
-
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ThreadEx2 even=new ThreadEx2() {
-		
-		public void run() {
-			for(int i=0;i<20;i++) {
-				if(i%2==0) {
-					System.out.println(i);
-				}
-			}
-		}
-		
-		
-	};
-	ThreadEx2 odd=new ThreadEx2() {
-		public void run() {
-			for(int i=0;i<20;i++) {
-				if(i%2!=0) {
-					System.out.println(i);
-				}
-			}
-		}
-	};
-
+class EvenThread extends Thread {
+    public void run() {
+        for (int i = 2; i <= 20; i++) {
+        	if(i%2==0) {
+            System.out.println("Even: " + i);
+        	}
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
 }
+
+class OddThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 20; i++) {
+        	if(i%2!=0) {
+            System.out.println("Odd: " + i);
+        	}
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
+
+public class ThreadEx2 {
+    public static void main(String[] args) {
+        EvenThread evenThread = new EvenThread();
+        OddThread oddThread = new OddThread();
+
+        evenThread.start();
+        oddThread.start();
+    }
 }
