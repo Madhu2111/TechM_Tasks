29e3d1456b4138240a9096e43a3691e3b8639b73
madhudamsani <126312099+Madhu2111@users.noreply.github.com>
2025-02-04 22:24:03 +0530

    Add files via upload

---
+++ 04-02-2025/Collections/CollectionTask5.java
@@ -0,0 +1,26 @@
+package com.collection.examples;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CollectionTask5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int n=sc.nextInt();
+		ArrayList<Integer> list=new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
